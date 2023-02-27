020cd6b92d47f505159a13088947d87c2be5456f
itayk21 <itayk21@gmail.com>
2023-02-27 22:14:20 +0200

    Add app router

---
+++ src/AppRouter.jsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import { Route, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  );
+};
