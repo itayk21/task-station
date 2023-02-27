3c8cc88c98c6b0b2770050fe764e755a5fdfd36c
itayk21 <itayk21@gmail.com>
2023-02-27 22:42:53 +0200

    WIP

--- src/components/layout/Base.jsx
+++ src/components/layout/Base.jsx
@@ -1,6 +1,24 @@
 import { Sidebar } from "./Sidebar/Sidebar";
+import { auth } from "../../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
+import { findUserById } from "../../lib/firebase/actions";
 
 export const BaseLayout = ({ children }) => {
+  const [user, loading, error] = useAuthState(auth);
+  const [userData, setUserData] = useState(false);
+
+  useEffect(() => {
+    // user UID that we are getting from firebase SDK
+    const userUID = user?.uid;
+
+    if (userUID) {
+      // when we having the user UID,
+      // we fetching to see his data from database
+      findUserById(userUID, setUserData);
+    }
+  }, [user]);
+
   return (
     <>
       <Sidebar />
