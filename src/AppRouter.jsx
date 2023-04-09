992ab48ff27ae29d3786f753942ffa22357200e1
itayk21 <itayk21@gmail.com>
2023-04-09 22:15:21 +0300

    WIP

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,12 +3,21 @@ import { BaseLayout } from "./components/layout/Base";
 import { Main } from "./components/main/Main";
 import "./App.css";
 import Profile from "./components/profile/Profile";
+import Active_workers from "./components/body/Active_workers";
+import React from "react";
+import Contact from "./components/body/Contact";
+import AddUser from "./components/users/AddUser";
+import Management from "./components/management/Management";
 
 export const AppRouter = () => {
   return (
     <BaseLayout>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/workers" element={<Active_workers />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/addUser" element={<AddUser />} />
+        <Route path="/management" element={<Management />} />
         <Route path="/profile/:id" element={<Profile />} />
       </Routes>
     </BaseLayout>
